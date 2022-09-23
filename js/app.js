eda0331a579422d0d5344a1fd34940abffb3ec18
Dario Argañaraz <dario.arganaraz@gmail.com>
2022-09-23 15:37:26 -0300

    App React without dev env

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict'; //
+
+const ReactElement = React.createElement;
+
+export default class Button extends React.Component {
+    render() {
+        return ReactElement(
+            'button',
+            {},
+            'Super Boton'
+        );
+    }
+}
+
+const domContainer = document.getElementById("myApp");
+ReactDOM.render(ReactElement(Button) , domContainer);
